5ec2571352b9d3e58f5be26b5fb66cbedf62fade
Lola Guerin <lola.guerin@gmail.com>
2020-11-09 17:23:08 +0000

    Ctreated the Pet model and Booking model, and tested: it works

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_09_170148) do
+ActiveRecord::Schema.define(version: 2020_11_09_172009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "date", default: "2020-11-09"
+    t.bigint "user_id", null: false
+    t.bigint "pet_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["pet_id"], name: "index_bookings_on_pet_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "pets", force: :cascade do |t|
+    t.string "name"
+    t.string "species"
+    t.bigint "pet_owner_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["pet_owner_id"], name: "index_pets_on_pet_owner_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +49,7 @@ ActiveRecord::Schema.define(version: 2020_11_09_170148) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "pets"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "pets", "users", column: "pet_owner_id"
 end
