8c2d269d53bbc1bf3c6b1fa1410f0b0e01168004
Lucien Andrieux <lucien.andrieux@epfedu.fr>
2020-11-10 10:40:06 +0000

    new and create methods added in booking_controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,21 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(bookings_params)
+
+    if @booking.save
+      redirect_to booking_path(@booking), notice: "New booking on #{@booking.date} registred successfully."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def bookings_params
+    params.require(:booking).permit(:date, :user_id, :pet_id)
+  end
 end
