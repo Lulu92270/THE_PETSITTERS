2d781707c29df26e2ed84da07e4eddc642417f32
marion gayon <mariongayon@gmail.com>
2020-11-10 11:04:23 +0000

    correction

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -19,6 +19,6 @@ class PetsController < ApplicationController
   private
 
   def pet_params
-    params.require(:pet).permit(:name, :spicies, :pet_owner_id)
+    params.require(:pet).permit(:name, :species, :pet_owner_id, :photo)
   end
 end
