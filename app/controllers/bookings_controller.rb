d459b33f9362d42eb5c3a9ccf5f444ac12fe8949
Lucien Andrieux <lucien.andrieux@epfedu.fr>
2020-11-12 15:36:55 +0000

    finishing book-new

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,19 +1,27 @@
-class BookingsController < ApplicationController
+class BookingsController < ApplicationController  
+  before_action :set_pet
+
   def new
     @booking = Booking.new
   end
 
   def create
     @booking = Booking.new(bookings_params)
+    @booking.pet = @pet
+    @booking.user = current_user
 
     if @booking.save
-      redirect_to booking_path(@booking), notice: "New booking on #{@booking.date} registred successfully."
+      redirect_to pets_path(@pet), notice: "New booking on #{@booking.date} for #{@pet.name} registred successfully."
     else
       render :new
     end
   end
 
   private
+
+  def set_pet
+    @pet = Pet.find(params[:pet_id])
+  end
 
   def bookings_params
     params.require(:booking).permit(:date, :user_id, :pet_id)
