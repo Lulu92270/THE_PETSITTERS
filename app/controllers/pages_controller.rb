9e4d3db07473175a6488abfda2a5ed2ebdaba8d4
Lucien Andrieux <lucien.andrieux@epfedu.fr>
2020-11-12 19:46:06 +0000

    seed + profile changed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,14 +5,13 @@ class PagesController < ApplicationController
   end
 
   def profile
-    @user = current_user
-    @user_bookings = Booking.where(user: @user)
+    @user_bookings = Booking.where(user: current_user)
+    @pets = Pet.where(pet_owner: current_user)
   end
 
   def change_role
-    @user = current_user
-    @user.role = params[:user][:role]
-    @user.save
+    current_user.role = params[:user][:role]
+    current_user.save
     redirect_to profile_path(current_user)
   end
 end
